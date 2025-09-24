7ebf00cffaf18096e4afe6a771f66fec7be8b476
Alexdoesvoices <50270256+Alexdoesvoices@users.noreply.github.com>
2025-09-24 11:53:59 -0500

    Inital commit

---
+++ nav.js
@@ -0,0 +1,25 @@
+fetch('nav.html')
+  .then(res => res.text())
+  .then(text => {
+    let oldelem = document.querySelector("script#replace_with_navbar");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem, oldelem);
+
+    // After nav is loaded, highlight the active link
+    let currentPage = window.location.pathname.split("/").pop();
+
+    // Handle case when no page is specified (just "/")
+    if (currentPage === "") {
+      currentPage = "index.html"; // Assume home page
+    }
+
+    let navLinks = document.querySelectorAll('ul.nav a');
+
+    navLinks.forEach(link => {
+      let linkPage = link.getAttribute('href');
+      if (linkPage === currentPage) {
+        link.classList.add('nav_active');
+      }
+    });
+  });
